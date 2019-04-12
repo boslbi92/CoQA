0df83fc3430d57d67c024c08e681e2d0bd10002e
boslbi92 <boslbi92@gmail.com>
2019-04-12 05:45:32 -0400

    gpu

--- HenNet/gpu_test.py
+++ HenNet/gpu_test.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
+from keras import backend as K
 
 a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
 b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
 c = tf.matmul(a, b)
-
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
 print(sess.run(c))
+print('tf version : ', tf.__version__)
+print(K.tensorflow_backend._get_available_gpus())
