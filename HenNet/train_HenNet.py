8fca123cc0f5101e1cc36f0308d6e27ad0e2d089
Jason <boslbi92@gmail.com>
2019-04-13 20:18:29 -0400

    update; ;

--- HenNet/train_HenNet.py
+++ HenNet/train_HenNet.py
@@ -1,5 +1,5 @@
 import numpy as np
-from model.model_HenNet import HenNet
+from model.model_HenNet2 import HenNet2
 from model.model_HenNet_GPU import HenNet_GPU
 from model.model_HenNet_GPU2 import HenNet_GPU2
 from keras.preprocessing.sequence import pad_sequences
@@ -57,7 +57,7 @@ def main():
     time.sleep(1.0)
     if args.g.lower() == 'false':
         print('Training HenNet on CPU mode...\n')
-        hn = HenNet(c_pad=args.c, h_pad=args.q, hidden_dim=args.d)
+        hn = HenNet2(c_pad=args.c, h_pad=args.q, hidden_dim=args.d)
         H = hn.build_model()
         H.fit_generator(train_generator, validation_data=([val_h_emb, val_c_emb], [val_targets]), epochs=50, steps_per_epoch=len(train_generator),
                         shuffle=True, use_multiprocessing=True, workers=6, callbacks=[monitor_span(), checkpoint, tensorboard])
